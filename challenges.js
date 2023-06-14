4313d71f48fe3237933fc769e0053aa85716eeea
Turabek <turabekyusubov587@gmail.com>
2023-06-15 02:35:14 +0500

    some solutions to edabit questions

---
+++ challenges.js
@@ -0,0 +1,40 @@
+// function sumPolygon(n){
+//     return (n-2) * 180
+// }
+// console.log(sumPolygon(3));
+// console.log(sumPolygon(4));
+// console.log(sumPolygon(6));
+
+// function nameString(string){
+//     return string + 'Edabit'
+// }
+// console.log(nameString("Mubashir"));
+// console.log(nameString("Javascript"));
+
+// function numArgs(){
+//     return arguments.length
+// }
+// console.log(numArgs({},"dlfpd",1));
+
+// function numArgs(...args){
+//     return args.length
+// }
+// console.log(numArgs({},"dlfpd",1));
+
+
+// function invertedArray(arr){
+//     return arr.map((i)=>i==0 ? 0 : i * -1)
+// }
+// console.log(invertedArray([1,2,3,4,5]));
+
+
+// function mod(a,b){
+//     return a % b
+// }
+// console.log(mod(5,2));
+// console.log(mod(218,5));
+
+// function mod2(a,b){
+//     return (a - b * parseInt(a / b))
+// }
+// console.log(mod2(5,2));
